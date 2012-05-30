f7df500f9ef4b2203a3abed219a31a21fbad5955
Richard Janicek <r@janicek.co>
2012-05-29 22:25:30 -0400

    -Minor updates.

--- README.md
+++ README.md
@@ -2,9 +2,11 @@ janicek-core-haxe
 =================
 
 My personal collection of Haxe core libraries.
+
 Functional style used where possible.
 
-Make sure to select "Recursive" option when cloning so libs are also cloned.
+Make sure you run ``git submodule update --init`` to clone the dependent libraries.
 
 https://github.com/rjanicek/janicek-core-haxe
+
 http://www.janicek.co
--- test/co/janicek/core/PathCoreSpec.hx
+++ test/co/janicek/core/PathCoreSpec.hx
@@ -10,16 +10,22 @@ class PathCoreSpec {
 
 	public function new() {
 		J.describe("PathCore", function() {
-			J.it("should get directory name from path that includes filename", function() {
-				J.expect(PathCore.getDirectoryName("a/b.txt")).toEqual("a");
+			J.describe("getDirectoryName", function(){
+				J.it("should get directory name from path that includes filename", function() {
+					J.expect(PathCore.getDirectoryName("a/b.txt")).toEqual("a");
+				});
 			});
 			
-			J.it("should get file name from path", function() {
-				J.expect(PathCore.getFileName("a/b.txt")).toEqual("b.txt");
+			J.describe("getFileName", function(){
+				J.it("should get file name from path", function() {
+					J.expect(PathCore.getFileName("a/b.txt")).toEqual("b.txt");
+				});
 			});
 			
-			J.it("should get file name without exentsion from path", function() {
-				J.expect(PathCore.getFileNameWithoutExtension("a/b.txt")).toEqual("b");
+			J.describe("getFileNameWithoutExtension", function(){
+				J.it("should get file name without exentsion from path", function() {
+					J.expect(PathCore.getFileNameWithoutExtension("a/b.txt")).toEqual("b");
+				});
 			});
 		});
 
