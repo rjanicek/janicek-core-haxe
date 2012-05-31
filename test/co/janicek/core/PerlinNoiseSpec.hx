1c8726522771330a3def9e5b2bc5677e78d88955
Richard Janicek <r@janicek.co>
2012-05-31 17:33:34 -0400

    -Condensed perlin noise into single function.

--- test/co/janicek/core/PerlinNoiseSpec.hx
+++ test/co/janicek/core/PerlinNoiseSpec.hx
@@ -14,13 +14,13 @@ class PerlinNoiseSpec {
 		J.describe("PerlinNoise", function() {
 			J.describe("new( seed = 666, octaves = 4, falloff = 0.5 )", function() {
 				J.it("should make perlin noise maker", function() {
-					var pn = new PerlinNoise();
-					J.expect(pn).not.toBeNull();
+					//var pn = new PerlinNoise();
+					//J.expect(pn).not.toBeNull();
 				});
 			});
 			J.describe("make(width:Int, height:Int, _x:Float, _y:Float, _z:Float, ?_ ):Array<Array<Int>>", function() {				
 				J.it("should make perlin noise data", function() {
-					var data = new PerlinNoise().make(100, 100, 1.0, 1.0, 1.0);
+					var data = PerlinNoise.makePerlinNoise(100, 100, 1.0, 1.0, 1.0);
 					J.expect(data).not.toBeNull();
 				});
 			});
