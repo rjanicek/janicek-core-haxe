a1c87d4bf0359c118009dd0b25b558c9f89ac9ff
Richard Janicek <r@janicek.co>
2012-05-31 17:41:31 -0400

    -updated specs

--- test/co/janicek/core/PerlinNoiseSpec.hx
+++ test/co/janicek/core/PerlinNoiseSpec.hx
@@ -12,13 +12,7 @@ class PerlinNoiseSpec {
 
 	public function new() {
 		J.describe("PerlinNoise", function() {
-			J.describe("new( seed = 666, octaves = 4, falloff = 0.5 )", function() {
-				J.it("should make perlin noise maker", function() {
-					//var pn = new PerlinNoise();
-					//J.expect(pn).not.toBeNull();
-				});
-			});
-			J.describe("make(width:Int, height:Int, _x:Float, _y:Float, _z:Float, ?_ ):Array<Array<Int>>", function() {				
+			J.describe("makePerlinNoise(width:Int, height:Int, _x:Float, _y:Float, _z:Float, seed = 666, octaves = 4, falloff = 0.5, ?_  ) : Array<Array<Int>>", function() {				
 				J.it("should make perlin noise data", function() {
 					var data = PerlinNoise.makePerlinNoise(100, 100, 1.0, 1.0, 1.0);
 					J.expect(data).not.toBeNull();
