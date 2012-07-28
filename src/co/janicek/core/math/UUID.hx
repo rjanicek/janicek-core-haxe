3df7b936d7273cf15212cea6158f4555bb90e728
Richard Janicek <r@janicek.co>
2012-07-28 11:33:10 -0400

    -improved UUID docs and specs

--- src/co/janicek/core/math/UUID.hx
+++ src/co/janicek/core/math/UUID.hx
@@ -17,36 +17,34 @@ Haxe conversion by Richard Janicek
 Dual licensed under the MIT and GPL licenses.
 */
 
-/*
- * Random UUID generators.
+/**
+ * Random universally unique identifier (UUID) generators.
+ * @see <a href="http://en.wikipedia.org/wiki/Uuid">http://en.wikipedia.org/wiki/Uuid</a>
  */
 class UUID {
 
 	private static var CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
   
 	/**
-	 * Generate a random uuid.
+	 * Generate a UUID.
 	 * 
 	 * EXAMPLES:
-	 *   // No arguments  - returns RFC4122, version 4 ID
-	 *   >>> Math.uuid()
-	 *   "92329D39-6F5C-4520-ABFC-AAB64544E172"
 	 *
-	 *   // One argument - returns ID of the specified length
-	 *   >>> Math.uuid(15)     // 15 character ID (default base=62)
+	 *   One argument - returns ID of the specified length
+	 *   >>> uuid(15)     // 15 character ID (default base=62)
 	 *   "VcydxgltxrVZSTV"
 	 *
-	 *   // Two arguments - returns ID of the specified length, and radix. (Radix must be <= 62)
-	 *   >>> Math.uuid(8, 2)  // 8 character ID (base=2)
+	 *   Two arguments - returns ID of the specified length, and radix. (Radix must be <= 62)
+	 *   >>> uuid(8, 2)  // 8 character ID (base=2)
 	 *   "01001010"
-	 *   >>> Math.uuid(8, 10) // 8 character ID (base=10)
+	 *   >>> uuid(8, 10) // 8 character ID (base=10)
 	 *   "47473046"
-	 *   >>> Math.uuid(8, 16) // 8 character ID (base=16)
+	 *   >>> uuid(8, 16) // 8 character ID (base=16)
 	 *   "098F4D35"
 	 * 
-	 * @param	?length the desired number of characters
-	 * @param	?radix the number of allowable values for each character, must be <= 62
-	 * @param	?seed optional PRNG seed
+	 * @param	length the desired number of characters
+	 * @param	radix the number of allowable values for each character, clamped to [2,62], default = 62
+	 * @param	seed optional PRNG seed
 	 */
 	public static function uuid( length : Int, ?radix : Int, ?seed : Int ) {
 		
@@ -59,7 +57,12 @@ class UUID {
 		return uuid.join("");
 	}
 	
-	public static function uuidRfc4122( ?seed : Int ) {
+	/**
+	 * generate RFC4122, version 4 ID
+	 * example "92329D39-6F5C-4520-ABFC-AAB64544E172"
+	 * @param	seed optional PRNG seed
+	 */
+	public static function uuidRfc4122V4( ?seed : Int ) {
 		
 		if (seed.isNull()) seed = RandomCore.makeRandomSeed();
 		var chars = CHARS, uuid = [], i;
--- test/specs/co/janicek/core/math/UUIDSpec.hx
+++ test/specs/co/janicek/core/math/UUIDSpec.hx
@@ -20,9 +20,6 @@ class UUIDSpec {
 					J.expect(UUID.uuid(1).length).toEqual(1);
 					J.expect(UUID.uuid(10).length).toEqual(10);
 				});
-			});
-			
-			J.describe("uuid()", function() {
 				J.it("should make a uuid of specific radix", function() {
 					var uuid = UUID.uuid(10, 2);
 					J.expect(uuid.length).toEqual(10);
@@ -30,9 +27,17 @@ class UUIDSpec {
 				});
 			});
 			
-			J.describe("uuidRfc4122()", function() {
+			J.describe("uuidRfc4122V4()", function() {
+				var uuid;
 				J.it("should make a uuid", function() {
-					J.expect(UUID.uuidRfc4122()).toBeDefined();
+					uuid = UUID.uuidRfc4122V4();
+					J.expect(uuid).toBeDefined();
+				});
+				J.it("should be 36 characters long", function() {
+					J.expect(uuid.length).toEqual(36);
+				});
+				J.it("should have 5 parts seperated by hyphens", function() {
+					J.expect(uuid.split("-").length).toEqual(5);
 				});
 			});
 			
